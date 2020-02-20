ab40abf4ce22c183b55f209d6454d9dc1206b87e
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2020-02-20 14:22:59 +0200

    add activity for choose correct answer

---
+++ app/src/main/java/learning/mahmoudmabrok/englishtime/feature/feature/chooseCorrectAnswer/ChooseCorrectAnswer.kt
@@ -0,0 +1,13 @@
+package learning.mahmoudmabrok.englishtime.feature.feature.chooseCorrectAnswer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import learning.mahmoudmabrok.englishtime.R
+
+class ChooseCorrectAnswer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_correct_answer)
+    }
+}
