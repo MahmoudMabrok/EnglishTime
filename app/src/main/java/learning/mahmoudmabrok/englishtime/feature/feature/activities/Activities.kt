e62cba687763969be7bad9e529f9c3cd0ff3615b
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2020-03-14 22:12:44 +0200

    two player work woth miss character

---
+++ app/src/main/java/learning/mahmoudmabrok/englishtime/feature/feature/activities/Activities.kt
@@ -0,0 +1,161 @@
+package learning.mahmoudmabrok.englishtime.feature.feature.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nightonke.boommenu.BoomButtons.BoomButton
+import com.nightonke.boommenu.BoomButtons.HamButton
+import com.nightonke.boommenu.OnBoomListener
+import kotlinx.android.synthetic.main.activity_main.*
+import learning.mahmoudmabrok.englishtime.R
+import learning.mahmoudmabrok.englishtime.feature.feature.aorb.IsAOrB
+import learning.mahmoudmabrok.englishtime.feature.feature.categorizeWords.CategorizeWords
+import learning.mahmoudmabrok.englishtime.feature.feature.circleOdd.CircleOdd
+import learning.mahmoudmabrok.englishtime.feature.feature.collectWordWithCross.CollectWord
+import learning.mahmoudmabrok.englishtime.feature.feature.crossword.CrossWord
+import learning.mahmoudmabrok.englishtime.feature.feature.grammer.GrammerActivity
+import learning.mahmoudmabrok.englishtime.feature.feature.listenSelect.ListenAndSelect
+import learning.mahmoudmabrok.englishtime.feature.feature.puncuate.Puncate
+import learning.mahmoudmabrok.englishtime.feature.feature.selectPath.SelectGameType
+import learning.mahmoudmabrok.englishtime.feature.feature.showVocabs.ShowVocabs
+import learning.mahmoudmabrok.englishtime.feature.feature.snake.SnakeStair
+import learning.mahmoudmabrok.englishtime.feature.utils.Constants
+
+class Activities : AppCompatActivity() {
+
+
+    internal var unit = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = intent
+        unit = intent.getIntExtra(Constants.UNIT, 0)
+
+
+        val names = arrayOf("Complete Miss", "Punctuate", "Grammar", "Structures", "Collect Words")
+        for (i in 0 until bmb.piecePlaceEnum.pieceNumber()) {
+            val builder = HamButton.Builder()
+                    .normalText(names[i])
+            bmb!!.addBuilder(builder)
+        }
+
+        bmb!!.onBoomListener = object : OnBoomListener {
+            override fun onClicked(index: Int, boomButton: BoomButton) {
+                when (index) {
+                    0 -> completeWord()
+                    1 -> puncate()
+                    2 -> openGrammer()
+                    3 -> openIsA()
+                    4 -> openWords()
+                    5 -> onViewClickedSnake()
+                }
+            }
+
+            override fun onBackgroundClick() {
+
+            }
+
+            override fun onBoomWillHide() {
+
+            }
+
+            override fun onBoomDidHide() {
+
+            }
+
+            override fun onBoomWillShow() {
+
+            }
+
+            override fun onBoomDidShow() {
+
+            }
+        }
+    }
+
+    private fun openWords() {
+        val openAcivity = Intent(this@Activities, SelectGameType::class.java)
+        openAcivity.putExtra(Constants.TYPE, Constants.CATEGORIES)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    private fun openIsA() {
+        val openAcivity = Intent(this@Activities, IsAOrB::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    private fun puncate() {
+        val openAcivity = Intent(this@Activities, Puncate::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    private fun openGrammer() {
+        val openAcivity = Intent(this@Activities, GrammerActivity::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    private fun completeWord() {
+        val openAcivity = Intent(this@Activities, SelectGameType::class.java)
+        openAcivity.putExtra(Constants.TYPE, Constants.WORDS)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        bmb!!.isAutoBoom = true
+    }
+
+    fun onButtonVocabClicked() {
+        val openAcivity = Intent(this@Activities, ShowVocabs::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    fun onButtonFormClicked() {
+        val openAcivity = Intent(this@Activities, CategorizeWords::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    fun onButtonOddClicked() {
+        val openAcivity = Intent(this@Activities, CircleOdd::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    fun onButtonListenClicked() {
+        val openAcivity = Intent(this@Activities, ListenAndSelect::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+
+    fun onViewClicked() {
+        val openAcivity = Intent(this@Activities, CrossWord::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+
+    fun onViewClickedSnake() {
+        val openAcivity = Intent(this@Activities, SnakeStair::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+    fun collectWords() {
+        val openAcivity = Intent(this@Activities, CollectWord::class.java)
+        openAcivity.putExtra(Constants.UNIT, unit)
+        startActivity(openAcivity)
+    }
+
+
+}
---
+++ app/src/main/java/learning/mahmoudmabrok/englishtime/feature/feature/categorizeWords/CategorizeWordsTwo.kt
@@ -0,0 +1,257 @@
+package learning.mahmoudmabrok.englishtime.feature.feature.categorizeWords
+
+import android.os.Build
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_categorize_words_two.*
+import kotlinx.android.synthetic.main.activity_complete_word_two.cardOne
+import kotlinx.android.synthetic.main.activity_complete_word_two.cardTwo
+import kotlinx.android.synthetic.main.activity_complete_word_two.playerName1
+import kotlinx.android.synthetic.main.activity_complete_word_two.playerName2
+import kotlinx.android.synthetic.main.activity_complete_word_two.playerScore1
+import kotlinx.android.synthetic.main.activity_complete_word_two.playerScore2
+import kotlinx.android.synthetic.main.activity_form_sentence.rvAllWords
+import kotlinx.android.synthetic.main.activity_form_sentence.rvCategory
+import kotlinx.android.synthetic.main.activity_form_sentence.tvCategoryName
+import kotlinx.android.synthetic.main.activity_form_sentence.tvScoreForm
+import kotlinx.android.synthetic.main.names.*
+import learning.mahmoudmabrok.englishtime.R
+import learning.mahmoudmabrok.englishtime.feature.datalayer.DataSet
+import learning.mahmoudmabrok.englishtime.feature.datalayer.LocalDB
+import learning.mahmoudmabrok.englishtime.feature.datalayer.models.Category
+import learning.mahmoudmabrok.englishtime.feature.utils.Constants
+import learning.mahmoudmabrok.englishtime.feature.utils.dismissKeyboard
+import learning.mahmoudmabrok.englishtime.feature.utils.isSame
+import learning.mahmoudmabrok.englishtime.feature.utils.setValue
+import java.util.*
+
+
+class CategorizeWordsTwo : AppCompatActivity() {
+
+    private lateinit var db: LocalDB
+    private val TAG: String = "CategorizeWords"
+    private val adapterTop: CategoryAdapter = CategoryAdapter()
+    private val adapterBottom: CategoryAdapter = CategoryAdapter()
+
+    var currentSentence = 0
+
+    var score = 0
+    lateinit var textToSpeech: TextToSpeech
+
+    private lateinit var categories: List<Category>
+    private lateinit var currentCategory: Category
+
+
+    var score1 = 0
+    var score2 = 0
+
+    var name1 = ""
+    var name2 = ""
+
+    var currentPlayer = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorize_words_two)
+        initRv()
+        setUpSentences()
+        loadSentence()
+
+        tvScoreForm.setMessage("Score:: ")
+
+
+        textToSpeech = TextToSpeech(this, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR) {
+                textToSpeech.language = Locale.ENGLISH
+            }
+        })
+
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+
+
+        btnPlayTwo.setOnClickListener {
+            name1 = edPlayerOneName.text.toString()
+            name2 = edPlayerTwoName.text.toString()
+
+            namesCat.visibility = View.GONE
+
+            content.visibility = View.VISIBLE
+
+            updateNames()
+            updateScores()
+
+            currentPlayer = 1
+
+            updatePlayerBoardUI()
+
+            this.dismissKeyboard()
+
+        }
+
+        btnSunmitCat.setOnClickListener {
+            check()
+        }
+
+
+    }
+
+    private fun check() {
+
+        // check if top is correct
+        if (currentCategory.getWords().isSame(adapterTop.list)) {
+            // point to next item
+            currentSentence += 1
+            // load new challenge
+            loadSentence()
+        }
+
+
+    }
+
+    private fun updatePlayerBoardUI() {
+        updateScores()
+        // current
+        if (currentPlayer == 1) {
+            cardOne.setBackgroundResource(R.drawable.collect_selected)
+            cardTwo.setBackgroundResource(R.drawable.collect_unselected)
+        } else {
+            cardTwo.setBackgroundResource(R.drawable.collect_selected)
+            cardOne.setBackgroundResource(R.drawable.collect_unselected)
+        }
+    }
+
+    private fun updateScores() {
+        playerScore1.text = score1.toString()
+        playerScore2.text = score2.toString()
+    }
+
+    private fun updateNames() {
+        playerName1.text = name1
+        playerName2.text = name2
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        loadScore()
+    }
+
+    private fun loadScore() {
+        db = LocalDB.getINSTANCE(this)
+        score = db.score
+        tvScoreForm.setValue(score, 1500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        db.score = score
+    }
+
+    private fun initRv() {
+        // make rv to be filled from user
+        rvCategory.setHasFixedSize(true)
+        rvCategory.adapter = adapterTop
+
+
+        // make given rv
+        rvAllWords.setHasFixedSize(true)
+        rvAllWords.adapter = adapterBottom
+
+        // listener for each rv
+        adapterBottom.setSentenceListener { pos, item ->
+            handleCLickToBottomRV(pos, item)
+        }
+
+        adapterTop.setSentenceListener { pos, item ->
+            adapterTop.removeSentence(pos)
+            adapterBottom.addSentence(item)
+        }
+
+    }
+
+    private fun handleCLickToBottomRV(pos: Int, item: String) {
+        speakWord(item)
+        adapterBottom.removeSentence(pos)
+        // add it to top rv
+        adapterTop.addSentence(item)
+    }
+
+    private fun speakWord(item: String) {
+        textToSpeech.speak(item, TextToSpeech.QUEUE_FLUSH, null)
+
+    }
+
+
+    private fun setUpSentences() {
+        if (intent.hasExtra(Constants.UNIT)) {
+            categories = DataSet.getCategory(intent.getIntExtra(Constants.UNIT, 0))
+            laodDataOfAllWords()
+        } else {
+            categories = DataSet.getCategory(0)
+            laodDataOfAllWords()
+            finish()
+        }
+
+    }
+
+    /**
+     * load all words and place them in bottom rv
+     */
+    private fun laodDataOfAllWords() {
+        val res = categories.flatMap { it.getWords() }
+        adapterBottom.setSentenceList(res)
+
+        /*val last:Category = categories.filter { it.name == "NA" }.first()
+        categories.toMutableList().remove(last)
+        */
+    }
+
+    /**
+     * Load Category
+     * clear top rv to add new words
+     * get current category and place name to view
+     * if items in categories end it finish activity
+     */
+    private fun loadSentence() {
+        try {
+            // clear them first
+            adapterTop.clear()
+            if (currentSentence == categories.size - 1) {
+                finish()
+            }
+            currentCategory = categories.get(currentSentence)
+            // set name to view
+            tvCategoryName.text = currentCategory.name
+
+        } catch (e: Exception) {
+            Log.v(TAG, e.localizedMessage)
+            finish()
+        }
+    }
+
+    private fun vibrate() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            (getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            (getSystemService(VIBRATOR_SERVICE) as Vibrator).vibrate(150)
+        }
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        try {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+}
